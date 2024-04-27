fade1d4a983be5ddf67bb36d092c6b65219137e0
22026526-san <sangv6548@gmail.com>
2024-04-27 20:19:58 +0700

    new data

--- src/models/connectSQL.js
+++ src/models/connectSQL.js
@@ -4,13 +4,9 @@ const connection = mysql.createConnection({
     host: 'localhost', // Địa chỉ của máy chủ cơ sở dữ liệu
     port: 3306,
     user: 'root', // Tên người dùng của cơ sở dữ liệu
-<<<<<<< HEAD
     password: '5ang15122003', // Mật khẩu của cơ sở dữ liệu
     database: 'web' // Tên của cơ sở dữ liệu
-=======
-    password: 'Ducquangk6@', // Mật khẩu của cơ sở dữ liệu
-    database: 'app' // Tên của cơ sở dữ liệu
->>>>>>> 06ee56a23e4fd091e7a38ce2537d6ae3443b6491
+
   });
   
 // Kết nối đến cơ sở dữ liệu
