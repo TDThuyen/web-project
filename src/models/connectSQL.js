70f97e8f0ae15b7fb09007ff33baab053f9215b5
Ta Duy Thuyen <22026505@vnu.edu.vn>
2024-04-28 23:54:15 +0700

    new update

--- src/models/connectSQL.js
+++ src/models/connectSQL.js
@@ -4,8 +4,8 @@ const connection = mysql.createConnection({
     host: 'localhost', // Địa chỉ của máy chủ cơ sở dữ liệu
     port: 3306,
     user: 'root', // Tên người dùng của cơ sở dữ liệu
-    password: '5ang15122003', // Mật khẩu của cơ sở dữ liệu
-    database: 'web' // Tên của cơ sở dữ liệu
+    password: 'taduythuyendaibangxanh2771162004', // Mật khẩu của cơ sở dữ liệu
+    database: 'thuyendepzai' // Tên của cơ sở dữ liệu
   });
   
   // Kết nối đến cơ sở dữ liệu
